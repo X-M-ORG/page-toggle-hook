20554d0a8eab5d7d038c0606d649eb63653ead67
chendu <chendu@wepie.com>
2021-01-29 14:22:35 +0800

    v0.0.4 修复微信小程序上执行的一些错误

--- src/env/miniapp.js
+++ src/env/miniapp.js
@@ -1,11 +1,12 @@
 export default function ({ onShow, onHide, watcherShow, watcherHide }) {
+  let isHide = false
+
   window.addEventListener('hashchange', () => {
-    let back = false
     watcherShow.forEach((vm) => {
       // 微信小程序更新 hash 会导致 history 增加一层，故需要 history.back() 一次
-      if (!back) {
+      if (isHide) {
         history.back()
-        back = true
+        isHide = false
       }
       vm.$options[onShow].call(vm)
     })
@@ -16,7 +17,8 @@ export default function ({ onShow, onHide, watcherShow, watcherHide }) {
     navigateTo.call(wx.miniProgram, {
       ...options,
       success(res) {
-        options.success(res)
+        isHide = true
+        options.success && options.success(res)
         watcherHide.forEach((vm) => vm.$options[onHide].call(vm))
       }
     })
